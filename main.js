d25b55be6466f34e21fce7387d0e517214839116
EmanuelVargas11 <vargasriquelmege@gmail.com>
2025-02-28 13:30:12 -0300

    Actualizacion del boton eliminar todo a eliminar todas las tareas

--- main.js
+++ main.js
@@ -1,33 +1,32 @@
 const formularioTareas = document.querySelector('#formulario-tareas');
 const inputIngresarTarea = document.querySelector('.input-ingresar-tarea');
 const btnAgregarTarea = document.querySelector('.btn-agregar-tarea');
-const btnEliminarTarea = document.querySelector('.btn-eliminar-tarea');
+const btnEliminarTarea = document.querySelector('.bbtn-eliminar-todas-las-tareas');
 const ulTareas = document.querySelector('.lista-tareas');
 const alerta = document.querySelector('.div');
-
+const contenedorTareas = document.querySelector('.contenedor-tareas');
+const body = document.querySelector('body');
+const btnEliminarTodasLasTareas = document.createElement('input');
+const lista = document.createElement('li');
 
 formularioTareas.addEventListener('submit', (e) => {
     e.preventDefault();
     const value = inputIngresarTarea.value;
-    // console.log(value);
-    
-    
+    console.log(value);
+    const li = document.createElement('li');   
     if (value) {
-        const li = document.createElement('li');
-        const button = document.createElement('button');
+        li.classList.add('li-tareas');     
         li.innerText = value;
-        ulTareas.append(li);
         alerta.classList.remove('alert-danger');
         alerta.innerText = '';
-        
-        agregarBtnRealizado(li, button);
-        btnEliminarTarea.addEventListener('click', () => {
-            // console.log('click');
-            ulTareas.remove(li)
-            
-            
-        })
+        // console.log(li, 'tarea agregada');
+        ulTareas.append(li);
+        ulTareas.classList.add('li-tareas');
+               
+        agregarBtnRealizado(li);
+      
     }else if (value === '') {
+        console.log("No hay datos");
         alerta.classList.add('alert-danger');
         alerta.classList.add('alert');
         alerta.innerText = 'Debes ingresar una tarea';  
@@ -35,19 +34,52 @@ formularioTareas.addEventListener('submit', (e) => {
     
     formularioTareas.reset()
     
+    eliminarTodasLasTarea(li)
 })
 
 
-function agregarBtnRealizado(li, button) {
-    
-    li.append(button);
-    button.innerText = 'Realizado'; 
-    button.classList.add('btn');
-    button.classList.add('btn-warning');
+function agregarBtnRealizado(li) {
+    const boton = document.createElement('button');
 
-    button.addEventListener('click', () => {
+    
+    li.append(boton);
+    boton.innerText = 'Tarea Realizada'; 
+    boton.classList.add('btn');
+    boton.classList.add('btn-warning');
+
+    boton.addEventListener('click', () => {
         li.remove();
+        // console.log('se elimino la tarea por medio del boton realizad');
     })
 
-    li.classList.add('list-group-item', 'd-flex', 'justify-content-between', 'align-items-center');
+    li.classList.add('lista-tareas' ,'list-group-item', 'd-flex', 'justify-content-between', 'align-items-center');
+}
+
+function eliminarTodasLasTarea(li) {
+    if (inputIngresarTarea.value === inputIngresarTarea.value) {
+        formularioTareas.append(btnEliminarTodasLasTareas);
+        btnEliminarTodasLasTareas.type = 'reset';
+        btnEliminarTodasLasTareas.value = 'Eliminar Todas Las Tareas';
+        btnEliminarTodasLasTareas.classList.add('btn-eliminar-todas-las-tareas', 'btn', 'btn-outline-danger', 'm-2', 'p-1,5', 'rounded-pill');
+    }else {
+        console.log('no se elimino todo');
+        ulTareas.append(li);
+        li.innerText = value;
+    agregarBtnRealizado(li);
+
+
+        
+    }
+
+        btnEliminarTodasLasTareas.addEventListener('click', () => {
+        ulTareas.remove(li);
+        // console.log('se elimino todo');
+        if (inputIngresarTarea.value === inputIngresarTarea.value) {
+            
+            btnEliminarTodasLasTareas.remove();
+        }
+        // console.log('eliminado');
+    })
+    
+    
 }
